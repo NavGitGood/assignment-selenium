1e67ed51b57224e2d134dfe3d4406c775c70d48a
Navneet Gupta <navneet.gupta@reedbusiness.com>
2021-05-04 00:40:11 +0530

    base test and configuration

---
+++ src/main/java/setup/ConfigurationLoader.java
@@ -0,0 +1,26 @@
+package setup;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * The setup.ConfigurationLoader class is used to read the properties file and provides a method
+ * to access different properties
+ */
+public class ConfigurationLoader {
+
+    private static Properties properties;
+    static {
+        try(InputStream is = ClassLoader.class.getResourceAsStream("/env.properties")) {
+            properties = new Properties();
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getPropertyValue(String key){
+        return properties.getProperty(key);
+    }
+}
